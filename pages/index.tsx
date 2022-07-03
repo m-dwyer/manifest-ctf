b6ed74ae9481d1a51f6ecf6e964e1fa4058db4df
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-07-03 17:29:50 +1000

    Add various eslint and prettier plugins/config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from 'next'
-import Link from 'next/link'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">ManifestCTF</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
