f508a97e1ce26a96e5e79762a1b2a96a841f2b37
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-07-10 16:37:11 +1000

    Add supabase and start of auth functionality

---
+++ components/LoginForm.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+export const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const handleLogin = async (email: string) => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signIn({ email });
+      if (error) throw error;
+      alert("Check your email for the login link!");
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="card w-96 bg-primary text-primary-content mx-auto">
+        <div className="card-body">
+          <div className="card-title">Log in</div>
+          <form className="form-control">
+            <label className="label" htmlFor="username">
+              email
+            </label>
+            <input
+              className="input"
+              type="text"
+              id="username"
+              name="username"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <label className="label" htmlFor="password">
+              password
+            </label>
+            <input
+              className="input"
+              type="password"
+              id="password"
+              name="password"
+            ></input>
+            <label className="label" htmlFor="confirmPassword">
+              confirm
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              className="input"
+            ></input>
+            <button className="btn mt-10" type="submit">
+              submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- components/SignupForm.tsx
+++ components/SignupForm.tsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+import { supabase } from "lib/supabaseClient";
+
 const SignupForm = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSignup = async (event: Event) => {
+    event.preventDefault();
+
+    if (confirmPassword !== password) {
+      alert("Passwords do not match");
+    }
+
+    const { error, session } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) setError(error.message);
+  };
+
   return (
     <div className="container mx-auto">
       <div className="card w-96 bg-primary text-primary-content mx-auto">
         <div className="card-body">
           <div className="card-title">Sign up</div>
-          <div>asd</div>
+          {error != null && <div>{error}</div>}
           <form className="form-control">
-            <label className="label" htmlFor="username">
-              username
+            <label className="label" htmlFor="email">
+              email
             </label>
             <input
               className="input"
-              type="text"
-              id="username"
-              name="username"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your email"
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
             <label className="label" htmlFor="password">
               password
@@ -23,6 +48,7 @@ const SignupForm = () => {
               type="password"
               id="password"
               name="password"
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
             <label className="label" htmlFor="confirmPassword">
               confirm
@@ -32,8 +58,13 @@ const SignupForm = () => {
               id="confirmPassword"
               name="confirmPassword"
               className="input"
+              onChange={(e) => setConfirmPassword(e.target.value)}
             ></input>
-            <button className="btn mt-10" type="submit">
+            <button
+              className="btn mt-10"
+              type="submit"
+              onClick={(e) => handleSignup(e)}
+            >
               submit
             </button>
           </form>
