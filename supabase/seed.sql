aecca20076ca9fa5b87e3f1cc62eb6c8ebda61fc
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-08-19 20:08:47 +1000

    add seeding categories

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,17 @@
--- seed challenges
 do $$
 begin
+-- seed categories
+insert into public.categories(
+    id,
+    created_at,
+    name
+) values (
+    nextval('categories_id_seq'),
+    now(),
+    'Default'
+);
+
+-- seed challenges
 for c in 1..50 loop
     insert into public.challenges(
         id,
@@ -17,6 +28,19 @@ for c in 1..50 loop
         'my_flag' || c,
         (array[10,20,30,40,50])[floor(random() * 5 + 1)]
     );
+    end loop;
+
+    -- seed challenge categories
+    for c in 1..50 loop
+        insert into public.challenge_categories(
+            created_at,
+            challenge,
+            category
+        ) values (
+            now(),
+            c,
+            1
+        );
     end loop;
 end;
 $$;
