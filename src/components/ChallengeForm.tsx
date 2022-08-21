ba1ca3ec9580b9b4d4ae1650ee5b8f79df3ca1b9
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-08-21 20:08:01 +1000

    Allow react global

--- src/components/ChallengeForm.tsx
+++ src/components/ChallengeForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 
 import { useMultiInputs } from "@lib/hooks/useMultiInputs";
