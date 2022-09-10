2b417220b510e30163cd02b39c725878def1022e
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-09-10 14:39:43 +1000

    Add ProfileDropdown tests

---
+++ src/base/components/ProfileDropdown.test.tsx
@@ -0,0 +1,47 @@
+import ProfileDropdown from "@/base/components/ProfileDropdown";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import singletonRouter from "next/router";
+import { act } from "react-test-renderer";
+
+jest.mock("@/base/queries/authentication", () => ({
+  __esModule: true,
+  logout: null,
+}));
+import * as auth from "@/base/queries/authentication";
+
+jest.mock("@supabase/auth-helpers-react", () => ({
+  __esModule: true,
+  useUser: null,
+}));
+import * as authHelper from "@supabase/auth-helpers-react";
+
+describe("ProfileDropdown", () => {
+  it("renders", () => {
+    const mockUseUser = authHelper as { useUser: unknown };
+    mockUseUser.useUser = () => ({ user: { email: "user@example.com" } });
+
+    render(<ProfileDropdown />);
+
+    const userEmailText = screen.getByText("user@example.com");
+    expect(userEmailText).toBeInTheDocument();
+  });
+
+  it("successfully logs out", () => {
+    const mockLogout = auth as { logout: unknown };
+    mockLogout.logout = jest.fn();
+
+    render(<ProfileDropdown />);
+
+    const logoutLink = screen.getByText("Logout");
+
+    act(() => {
+      fireEvent.click(logoutLink);
+    });
+
+    waitFor(() => {
+      expect(mockLogout.logout).toBeCalled();
+      expect(singletonRouter).toMatchObject({ asPath: "/" });
+    });
+  });
+});
