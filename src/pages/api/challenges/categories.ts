d2dd189924d78cc6bb49b8c627ca7d6d27dd2e66
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-10-23 17:50:56 +1100

    move basic category fetch endpoint to prisma

--- src/pages/api/challenges/categories.ts
+++ src/pages/api/challenges/categories.ts
@@ -1,37 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { withApiAuth } from "@supabase/auth-helpers-nextjs";
 import nc from "next-connect";
 import { fetchAllCategories } from "@/challenges/services/categoryAdmin";
 import { buildResponse } from "@/common/lib/ResponseBuilder";
-import type { ChallengeCategory } from "@/challenges/schemas/challengeCategory";
 import { ResponseWithData } from "@/common/types/ResponseWithData";
+import { Category } from "@prisma/client";
 
-export default withApiAuth(
-  nc<NextApiRequest, NextApiResponse>({
-    onError: (err, req, res, next) => {
-      console.error(err.stack);
-      res
+export default nc<NextApiRequest, NextApiResponse>({
+  onError: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json(buildResponse({ success: false, error: err.message }));
+  },
+}).get(
+  async (
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseWithData<Category[]>>
+  ) => {
+    const { data, error } = await fetchAllCategories();
+
+    if (error || data === null)
+      return res
         .status(500)
-        .json(buildResponse({ success: false, error: err.message }));
-    },
-  }).get(
-    async (
-      req: NextApiRequest,
-      res: NextApiResponse<ResponseWithData<ChallengeCategory[]>>
-    ) => {
-      const { data, error } = await fetchAllCategories();
+        .json(buildResponse({ success: false, error: error?.message }));
 
-      if (error || data === null)
-        return res
-          .status(500)
-          .json(buildResponse({ success: false, error: error?.message }));
-
-      res.status(200).json(
-        buildResponse<ChallengeCategory[]>({
-          success: true,
-          data: data || [],
-        })
-      );
-    }
-  )
+    res.status(200).json(
+      buildResponse<Category[]>({
+        success: true,
+        data: data || [],
+      })
+    );
+  }
 );
