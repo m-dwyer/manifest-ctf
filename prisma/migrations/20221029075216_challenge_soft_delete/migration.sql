58b9da46b7479bc3bd8416c9e8e84307a3632ee9
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2022-10-29 19:14:13 +1100

    add challenge soft deletion

---
+++ prisma/migrations/20221029075216_challenge_soft_delete/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Challenge" ADD COLUMN     "deleted" TIMESTAMP(3);
--- src/common/providers/prismaClient.ts
+++ src/common/providers/prismaClient.ts
@@ -12,4 +12,35 @@ export const prisma =
     log: ["query"],
   });
 
+// middleware
+
+prisma.$use(async (params, next) => {
+  if (params.model === "Challenge") {
+    if (params.action === "delete") {
+      params.action = "update";
+      params.args["data"] = { deleted: new Date() };
+    }
+  }
+
+  return next(params);
+});
+
+prisma.$use(async (params, next) => {
+  if (params.model === "Challenge") {
+    if (
+      params.action === "findMany" ||
+      params.action === "findUnique" ||
+      params.action === "findFirst"
+    ) {
+      if (params.args.where != undefined) {
+        params.args.where["deleted"] = null;
+      } else {
+        params.args["where"] = { deleted: null };
+      }
+    }
+  }
+
+  return next(params);
+});
+
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
