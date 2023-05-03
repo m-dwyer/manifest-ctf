3515cbabcfc365f18c2702954aa281635bb7912f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2023-05-03 17:21:21 +1000

    Add chart.js and mock chart to profile page

--- src/base/components/ProfilePage.tsx
+++ src/base/components/ProfilePage.tsx
@@ -1,7 +1,52 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+};
+
+const labels = ["Date 1", "Date 2", "Date 3", "Date 4"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Challenges completed",
+      data: [1, 2, 3, 4],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Points earned",
+      data: [100, 50, 200, 300],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
 const ProfilePage = () => {
   return (
     <section>
-      <h1>sdfsd</h1>
+      <h1>User Progress</h1>
+      <Line options={options} data={data} />
       <div className="flex-initial card w-96 bg-base-200 shadow-xl">
         <div className="card-body">
           <div className="card-title">
