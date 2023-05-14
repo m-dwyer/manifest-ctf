5c2b7eb8ecccfe3d128026af0bf23e481d22468f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2023-05-14 20:09:35 +1000

    update NavBar spec to test for existence of admin menu item

--- src/base/components/NavBar.test.tsx
+++ src/base/components/NavBar.test.tsx
@@ -4,16 +4,50 @@ import "@testing-library/jest-dom";
 import singletonRouter from "next/router";
 import { act } from "react-test-renderer";
 
+import { useSession } from "next-auth/react";
+const mockSession = {
+  expires: new Date(Date.now() + 2 * 86400).toISOString(),
+  user: { email: "foo@bar.com", role: "USER" },
+};
+jest.mock("next-auth/react", () => ({
+  __esModule: true,
+  useSession: jest.fn(() => {
+    return { data: mockSession, status: "authenticated" };
+  }),
+}));
+
+import * as useSessionHelper from "next-auth/react";
+
 /**
  * @group unit
  * @group components
  */
 describe("NavBar", () => {
   it("renders", () => {
+    const mockUserSession = useSessionHelper as { useSession: unknown };
+    mockUserSession.useSession = jest.fn(() => ({
+      data: { user: { email: "foo@bar.com", role: "USER" } },
+    }));
+
     render(<NavBar />);
 
     const challengesLink = screen.getByText("Challenges");
-    const challengesAdminLink = screen.getByText("Challenges Admin");
+    const challengesAdminLink = screen.queryByText("Challenges Admin");
+
+    expect(challengesLink).toBeInTheDocument();
+    expect(challengesAdminLink).not.toBeInTheDocument();
+  });
+
+  it("renders challenges admin with admin role", () => {
+    const mockUserSession = useSessionHelper as { useSession: unknown };
+    mockUserSession.useSession = jest.fn(() => ({
+      data: { user: { email: "foo@bar.com", role: "ADMIN" } },
+    }));
+
+    render(<NavBar />);
+
+    const challengesLink = screen.getByText("Challenges");
+    const challengesAdminLink = screen.queryByText("Challenges Admin");
 
     expect(challengesLink).toBeInTheDocument();
     expect(challengesAdminLink).toBeInTheDocument();
