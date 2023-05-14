b7e3f5bf7091ac3ae2b0cd6420ccc005ac3b2bb6
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2023-05-14 15:14:18 +1000

    Add next-auth User type to auth function

--- src/base/services/user.ts
+++ src/base/services/user.ts
@@ -1,15 +1,17 @@
 import { prisma } from "@/common/providers/prismaClient";
 import { compareSync } from "bcrypt";
 
+import { User } from "next-auth";
+
 export const authorizeUser = async ({
   email,
   password,
 }: {
   email?: string;
   password?: string;
-}) => {
+}): Promise<User> => {
   if (!email || !password) {
-    return null;
+    throw new Error("Invalid email or password");
   }
 
   const user = await prisma.user.findUnique({
@@ -24,7 +26,7 @@ export const authorizeUser = async ({
   });
 
   if (!user) {
-    return null;
+    throw new Error("Invalid email or password");
   }
 
   const checkPassword = compareSync(password, user.password);
@@ -33,5 +35,5 @@ export const authorizeUser = async ({
     return { id: user.id.toString(), email: email };
   }
 
-  return null;
+  throw new Error("Invalid email or password");
 };
